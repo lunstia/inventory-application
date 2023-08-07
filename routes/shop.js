0186c7493527c0835ebb14aae5422a6dad1098f8
lunstia <136163666+lunstia@users.noreply.github.com>
2023-08-06 20:01:34 -0400

    Revise route since HTML forms only use POST and GET I had to update the routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,20 +1,28 @@
 const categoryController = require('../controllers/categoryController')
+const Category = require('../models/category')
 
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop');
+// Shop's home page
+router.get('/', async function(req, res, next) {
+  const categories = await Category.find().exec()
+
+  if (categories[0]) {
+    res.redirect(`/shop/${categories[0].category}`);
+  } else {
+    next(new Error("ERROR: No categories"))
+  }
 });
 
+// Controller routes
 router.get('/:category', categoryController.category_read);
 
 router.post('/create', categoryController.category_create);
 
-router.put('/:category/update', categoryController.category_update);
+router.post('/:category/update', categoryController.category_update);
 
-router.delete('/:category/delete', categoryController.category_delete);
+router.post('/:category/delete', categoryController.category_delete);
 
 router.get('/:category/:id');
 
